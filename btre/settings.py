71a38dabf5d469cde59e491078614ce408f0328c
memadd <memad632@gmail.com>
2020-09-26 20:14:29 +0200

    removed email from settings

--- btre/settings.py
+++ btre/settings.py
@@ -141,12 +141,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'memad632@gmail.com'
-EMAIL_HOST_PASSWORD = 'Elomda88'
-EMAIL_USE_TLS = True
 
 try:
     from .local_settings import *
