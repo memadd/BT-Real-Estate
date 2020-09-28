da31cdd45c9ae6cbf83ce90f2b12f66131381749
memadd <46400214+memadd@users.noreply.github.com>
2020-09-28 20:37:13 +0200

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': 'Elomda88',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
